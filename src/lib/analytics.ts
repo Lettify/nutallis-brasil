326fd32858dd301aae356d3605e39f9d8c31b07d
Lettify <michael.gat.felipe@gmail.com>
2026-02-05 10:35:05 -0300

    delete all

--- src/lib/analytics.ts
+++
@@ -1,5 +0,0 @@
-export const getSinceIsoDate = (days: number) => {
-  const now = new Date();
-  const since = new Date(now.getTime() - days * 24 * 60 * 60 * 1000);
-  return since.toISOString();
-};
--- src/lib/currency.ts
+++
@@ -1,5 +0,0 @@
-export const formatCurrencyBRL = (valueCents: number) =>
-  new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  }).format(valueCents / 100);
